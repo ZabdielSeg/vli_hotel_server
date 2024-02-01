8730f440dc946d9add13955f12f35c7a39620184
Zabdiel Segura <zabdiel-segura@hotmail.com>
2024-02-01 11:13:29 -0600

    Add database environmental variable

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('postgres', 'zabdielsegura', "", {
-    host: "localhost",
-    dialect: "postgres"
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: true
+    }
 });
