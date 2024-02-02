3be3d336930efb371e9d463bc2562f20d7399c76
Zabdiel Segura <zabdiel-segura@hotmail.com>
2024-02-01 22:33:36 -0600

    Enable cors for cross communication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express';
+import cors from 'cors'
 import visitRoutes from './routes/visit.routes.js'
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
 
 app.use(visitRoutes)
 
